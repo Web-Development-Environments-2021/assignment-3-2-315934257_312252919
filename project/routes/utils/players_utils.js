a39f98710fa7ca24303f2e00ea8900fdfef2fb63
Guyzaidman <guyzaidm@gmail.com>
2021-05-22 17:43:57 +0300

    added player by id and by name

--- project/routes/utils/players_utils.js
+++ project/routes/utils/players_utils.js
@@ -16,7 +16,7 @@ async function getPlayerIdsByTeam(team_id) {
   return player_ids_list;
 }
 
-async function getPlayersInfo(players_ids_list) {
+async function getPlayersInfo(players_ids_list, full_details) {
   let promises = [];
   players_ids_list.map((id) =>
     promises.push(
@@ -29,7 +29,31 @@ async function getPlayersInfo(players_ids_list) {
     )
   );
   let players_info = await Promise.all(promises);
+  if(full_details){
+    return extractAllData(players_info);
+  }
   return extractRelevantPlayerData(players_info);
+}
+
+function extractAllData(players_info){
+  return players_info.map((player_info) => {
+    const { fullname, image_path, position_id,
+            common_name, nationality, birthdate,
+            birthcountry, height, weight } = player_info.data.data;
+    const { name } = player_info.data.data.team.data;
+    return {
+      name: fullname,
+      image: image_path,
+      position: position_id,
+      team_name: name,
+      common_name: common_name,
+      nationality: nationality,
+      birthdate: birthdate,
+      birthcountry: birthcountry,
+      height: height,
+      weight: weight
+    };
+  });
 }
 
 function extractRelevantPlayerData(players_info) {
@@ -45,11 +69,43 @@ function extractRelevantPlayerData(players_info) {
   });
 }
 
+function extractRelevantPlayerDataByName(players_info) {
+  let ret = []
+  players_info.map((player_info) => {
+    if(player_info.country_id === 320){
+      const { fullname, image_path, position_id } = player_info;
+      let name = null;
+      if(player_info.team){
+        name = player_info.team.data.name;
+      }
+
+      ret.push({
+        name: fullname,
+        image: image_path,
+        position: position_id,
+        team_name: name,
+      })
+    }
+  });
+  return ret;
+}
+
 async function getPlayersByTeam(team_id) {
   let player_ids_list = await getPlayerIdsByTeam(team_id);
-  let players_info = await getPlayersInfo(player_ids_list);
+  let players_info = await getPlayersInfo(player_ids_list, false);
   return players_info;
 }
 
+async function getPlayerDetailsByName(player_name){
+  const players = await axios.get(`${api_domain}/players/search/${player_name}`, {
+    params: {
+      api_token: process.env.api_token,
+      include: "team",
+    },
+  });
+  return extractRelevantPlayerDataByName(players.data.data);
+}
+
 exports.getPlayersByTeam = getPlayersByTeam;
+exports.getPlayerDetailsByName = getPlayerDetailsByName;
 exports.getPlayersInfo = getPlayersInfo;
