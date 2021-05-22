a2a61a3cb64af8696add64ee387880b4c62e4c03
JonatanMilver <48964784+JonatanMilver@users.noreply.github.com>
2021-05-22 14:46:20 +0300

    added creating DB tables queries and added vars in register auth

---
+++ project/DBQueries/create_tables.sql
@@ -0,0 +1,52 @@
+-- Create a new table called 'Users' in schema 'dbo'
+-- Drop the table if it already exists
+IF OBJECT_ID('dbo.Users', 'U') IS NOT NULL
+DROP TABLE dbo.Users
+GO
+-- Create the table in the specified schema
+CREATE TABLE dbo.Users
+(
+    -- primary key column
+    username [NVARCHAR](50) NOT NULL PRIMARY KEY,
+    first_name [NVARCHAR](50) NOT NULL,
+    last_name [NVARCHAR](50) NOT NULL,
+    country [NVARCHAR](50),
+    pswd [NVARCHAR](MAX) NOT NULL,
+    email [NVARCHAR](50) NOT NULL,
+    img_url [NVARCHAR](MAX),
+    favourites INT
+);
+GO
+
+-- -- Create a new table called 'Referees' in schema 'dbo'
+-- -- Drop the table if it already exists
+-- IF OBJECT_ID('dbo.Referees', 'U') IS NOT NULL
+-- DROP TABLE dbo.Referees
+-- GO
+-- -- Create the table in the specified schema
+-- CREATE TABLE dbo.Referees
+-- (
+--     referee_id INT NOT NULL PRIMARY KEY,
+--     first_name [NVARCHAR](50) NOT NULL,
+--     last_name [NVARCHAR](50) NOT NULL,
+--     age INT NOT NULL
+-- );
+-- GO
+
+-- -- Create a new table called 'Games' in schema 'dbo'
+-- -- Drop the table if it already exists
+-- IF OBJECT_ID('dbo.Games', 'U') IS NOT NULL
+-- DROP TABLE dbo.Games
+-- GO
+-- -- Create the table in the specified schema
+-- CREATE TABLE dbo.Games
+-- (
+--     -- primary key column
+--     game_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+--     home_team INT NOT NULL,
+--     away_team INT NOT NULL,
+--     game_date_time DATETIME NOT NULL,
+--     field INT NOT NULL,
+--     referee_id INT NOT NULL FOREIGN KEY REFERENCES Referees(referee_id)
+-- );
+-- GO
